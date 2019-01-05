d135a99f970eab0dfa8aeed6fef6bcabd4677584
ste-s <ste-s@PC-Ste>
2019-01-05 21:47:03 +0100

    modifications pour cloud endpoint

---
+++ TinyTwitt_Dussuet_Pagot/src/main/java/tinytwitt/TinyTwittEndpoint.java
@@ -0,0 +1,14 @@
+package tinytwitt;
+
+import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiMethod;
+import com.google.api.server.spi.config.ApiMethod.HttpMethod;
+
+@Api(
+	name= "tinytwitt",
+	version="v1",
+	description="API pour l'application TinyTwitt"
+	)
+public class TinyTwittEndpoint {
+
+}
