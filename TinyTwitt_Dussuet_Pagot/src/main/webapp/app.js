8a4ec318613c4c66e88210110df629250d717c87
InesDt <38463035+InesDt@users.noreply.github.com>
2019-01-06 23:40:30 +0100

    Update app.js

--- TinyTwitt_Dussuet_Pagot/src/main/webapp/app.js
+++ TinyTwitt_Dussuet_Pagot/src/main/webapp/app.js
@@ -3,9 +3,12 @@ var app = angular.module('tinytwitt', []);
 
 app.controller('UtilisateurControle',function($scope,$http){
 	$scope.iduser = 5629499534213120;
+	$scope.tempo = 0;
 	$scope.postAjoutUtilisateur = function(){
+		var temps = Date.now();
 		$http.put("http://localhost:8080/_ah/api/tinytwitt/v1/users/"+ $scope.nomuti).then(function successCallback(response){
-			
+			temps = Date.now() - temps;
+			$scope.tempo = temps;
 		}, function errorCallback(response){
 			console.log("envoi des données rate");
 		});
@@ -13,8 +16,11 @@ app.controller('UtilisateurControle',function($scope,$http){
 	}
 	
 	$scope.postAjoutMessage = function(){
+		var temps = Date.now();
 		$http.post("http://localhost:8080/_ah/api/tinytwitt/v1/messages/"+$scope.mcontenu+"/"+$scope.miduti+"/"+$scope.mdate).then(function successCallback(response){
 			$scope.getMessages();
+			temps = Date.now() - temps;
+			$scope.tempo = temps;
 		}, function errorCallback(response){
 			console.log("envoi des données rate");
 		});
@@ -23,33 +29,46 @@ app.controller('UtilisateurControle',function($scope,$http){
 		$scope.mdate="";
 	}
 	$scope.getMessages = function(){
+		var temps = Date.now();
 		$http.get("http://localhost:8080/_ah/api/tinytwitt/v1/messages/"+ idusermess).then(function(response){
 			$scope.data = response.data.items;
+			temps = Date.now() - temps;
+			$scope.tempo = temps;
 		});
 	}
 	$scope.getUsers = function(){
+		var temps = Date.now();
 		$http.get("http://localhost:8080/_ah/api/tinytwitt/v1/users").then(function(response){
 			$scope.dataUsers = response.data.items;
+			temps = Date.now() - temps;
+			$scope.tempo = temps;
 		});
 	}
 	$scope.getlistfollow = function(){
+		var temps = Date.now();
 		$http.get("http://localhost:8080/_ah/api/tinytwitt/v1/users/"+$scope.iduser+"/follows").then(function(response){
 			$scope.dataF = response.data.items;
+			temps = Date.now() - temps;
+			$scope.tempo = temps;
 		});
 	}
 	$scope.addfollow = function(){
+		var temps = Date.now();
 		$http.put("http://localhost:8080/_ah/api/tinytwitt/v1/users/"+$scope.iduser+"/follows/"+$scope.idaddf).then(function successCallback(response){
 			$scope.getlistfollow();
-			
+			temps = Date.now() - temps;
+			$scope.tempo = temps;
 		}, function errorCallback(response){
 			println("envoi des données rate");
 		});
 		$scope.idaddf="";
 	}
 	$scope.unfollow = function(){
+		var temps = Date.now();
 		$http.put("http://localhost:8080/_ah/api/tinytwitt/v1/users/"+$scope.iduser+"/unfollow/"+$scope.idunf).then(function successCallback(response){
 			$scope.getlistfollow();
-			
+			temps = Date.now() - temps;
+			$scope.tempo = temps;
 		}, function errorCallback(response){
 			console.log("envoi des données rate");
 		});
