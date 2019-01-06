4aed2a5dab8a944776a159ca5df44ad214b998b9
InesDt <38463035+InesDt@users.noreply.github.com>
2019-01-06 21:46:26 +0100

    Update app.js

--- TinyTwitt_Dussuet_Pagot/src/main/webapp/app.js
+++ TinyTwitt_Dussuet_Pagot/src/main/webapp/app.js
@@ -2,22 +2,26 @@ var app = angular.module('tinytwitt', []);
 
 
 app.controller('UtilisateurControle',function($scope,$http){
-	var idusermess = 5629499534213120;
+	$scope.iduser = 5629499534213120;
 	$scope.postAjoutUtilisateur = function(){
 		$http.put("http://localhost:8080/_ah/api/tinytwitt/v1/users/"+ $scope.nomuti).then(function successCallback(response){
-			$scope.nomuti="";
+			
 		}, function errorCallback(response){
-			console.log("envoi des données raté");
+			console.log("envoi des données rate");
 		});
+		$scope.nomuti="";
 	}
-	/**
+	
 	$scope.postAjoutMessage = function(){
-		$http.put("http://urlapi.com/message",{message:$scope.message,utilisateur:$scope.monuti}).then(function successCallback(response){
-			console.log("envoi des données réussi");
+		$http.post("http://localhost:8080/_ah/api/tinytwitt/v1/messages/"+$scope.mcontenu+"/"+$scope.miduti+"/"+$scope.mdate).then(function successCallback(response){
+			$scope.getMessages();
 		}, function errorCallback(response){
-			console.log("envoi des données raté");
+			console.log("envoi des données rate");
 		});
-	}*/
+		$scope.mcontenu="";
+		$scope.miduti="";
+		$scope.mdate="";
+	}
 	$scope.getMessages = function(){
 		$http.get("http://localhost:8080/_ah/api/tinytwitt/v1/messages/"+ idusermess).then(function(response){
 			$scope.data = response.data.items;
@@ -29,23 +33,33 @@ app.controller('UtilisateurControle',function($scope,$http){
 		});
 	}
 	$scope.getlistfollow = function(){
-		$http.get("http://localhost:8080/_ah/api/tinytwitt/v1/users/"+idusermess+"/follows").then(function(response){
+		$http.get("http://localhost:8080/_ah/api/tinytwitt/v1/users/"+$scope.iduser+"/follows").then(function(response){
 			$scope.dataF = response.data.items;
 		});
 	}
 	$scope.addfollow = function(){
-		$http.put("http://localhost:8080/_ah/api/tinytwitt/v1/users/"+idusermess+"/follows/"+$scope.idaddf).then(function successCallback(response){
-			$scope.idaddf="";
+		$http.put("http://localhost:8080/_ah/api/tinytwitt/v1/users/"+$scope.iduser+"/follows/"+$scope.idaddf).then(function successCallback(response){
+			$scope.getlistfollow();
+			
 		}, function errorCallback(response){
-			println("envoi des données raté");
+			println("envoi des données rate");
 		});
-		
+		$scope.idaddf="";
 	}
 	$scope.unfollow = function(){
-		$http.put("http://localhost:8080/_ah/api/tinytwitt/v1/users/"+idusermess+"/unfollow/"+$scope.idunf).then(function successCallback(response){
-			$scope.idunf="";
+		$http.put("http://localhost:8080/_ah/api/tinytwitt/v1/users/"+$scope.iduser+"/unfollow/"+$scope.idunf).then(function successCallback(response){
+			$scope.getlistfollow();
+			
 		}, function errorCallback(response){
-			console.log("envoi des données raté");
+			console.log("envoi des données rate");
 		});
+		$scope.idunf="";
 	}
+	/*
+	$scope.getHashtags = function(){
+		$http.get("http://localhost:8080/_ah/api/tinytwitt/v1/hashtags/"+$scope.hashtag).then(function(response){
+			$scope.dataH = response.data.items;
+		});
+	}*/
 });
+
